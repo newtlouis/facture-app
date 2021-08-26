bca7cf4a7ea6f239f17803df904ae11cf637d46c
Louis <louis.m.newton@hotmail.fr>
2021-08-26 22:56:49 +0200

    header

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,44 @@
 </template>
 
 <style lang="scss">
+
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+
+}
+
+button{
+  cursor: pointer;
+  padding: 16px 24px;
+  border-radius: 30px;
+  border: none;
+  font-size: 12px;
+  color: #FFF;
+}
+
+// COULEUR
+.dark-purple{
+  background-color: #252945;
+}
+.purple{
+  background-color: #7c5dfa;
+}
+
+.green{
+  background-color: #33d69f;
+}
+
+.orange{
+  background-color:#ff8f00;
 }
 
 #nav {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,76 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="header">
+      <div class="left">
+        <h1>Factures</h1>
+        <span>Il y a 4 factures</span>
+      </div>
+      <div class="right">
+        <div class="filter" @click="filter">
+          <span>Filtres:</span>
+          <ul v-show="showFilter" class="filter__list">
+            <li>Brouillons</li>
+            <li>En cours</li>
+            <li>Payées</li>
+            <li>Supprimer</li>
+          </ul>
+        </div>
+        <div @click="newInvoice" class="button">Nouvelle facture</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      showFilter : false,
+    }
+  },
+  methods:{
+    filter(){
+      if (this.showFilter == false){this.showFilter = true}
+      else {this.showFilter = false}
+      
+    }
   }
+  
 }
 </script>
+
+<style lang="scss" scoped>
+.header{
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 900px;
+  margin: 0 auto;
+}
+.left{
+  display: flex;
+  flex-direction: column;
+  justify-content: end;
+}
+.right{
+  display: flex;
+  justify-content: space-between;
+  width: 250px;
+}
+.filter{
+  cursor: pointer;
+}
+.fliter__list{
+  width: 120px;
+  position: absolute;
+  top: 25px;
+  list-style: none;
+
+  li{
+    cursor: pointer;
+    font-size: 12px;
+  }
+}
+
+</style>
