af51b5adb5bf907b457cecae6387f0520a2ce05d
Louis <louis.m.newton@hotmail.fr>
2021-08-27 21:07:13 +0200

    Modèle facture From To

---
+++ src/components/FactureModel.vue
@@ -0,0 +1,59 @@
+<template>
+    <div @click="checkClick" class="facture">
+        <form @submit.prevent="sendFacture">
+            <h1>Nouvelle Facture:</h1>
+            <div class="from">
+                <h3>Facture de:</h3>
+                <div class="input">
+                    <label for="billerAdress">Adresse</label>
+                    <input id="billerAdress" v-model="billerAdress" required type="text">
+                </div>
+                <div class="locationDetail">
+                    <div class="input">
+                    <label for="billerCity">Ville</label>
+                    <input id="billerCity" v-model="billerCity" required type="text">
+                </div>
+                <div class="input">
+                    <label for="billerCP">Code Postal</label>
+                    <input id="billerCP" v-model="billerCP" required type="text">
+                </div>
+                <div class="input">
+                    <label for="billerCountry">Pays</label>
+                    <input id="billerCountry" v-model="billerCountry" required type="text">
+                </div>
+                </div>
+            </div>
+        </form>
+        facture
+    </div>
+</template>
+
+<script>
+export default {
+    name:"factureModel",
+    data(){
+        return{
+            billerAdress: null,
+            billerCity: null,
+            billerCP: null,
+            billerCountry: null,
+            clientName: null,
+            clientEmail: null,
+            clientAdress: null,
+            clientCity: null,
+            clientCP: null,
+            clientCountry: null,
+            factureDate: null,
+            facturePending: null,
+            factureDraft: null,
+            factureList: [],
+            factureTotal: 0,
+            paymentDueDate: null,
+            productDescription: null,
+
+
+        }
+    }
+
+}
+</script>
